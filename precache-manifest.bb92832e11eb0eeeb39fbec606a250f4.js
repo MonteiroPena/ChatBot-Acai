0ea42bafdb45e3d9ee87a6e7c92f91783e7364f6
Daniel Monteiro <dmpassis@gmail.com>
2019-12-11 15:31:23 -0300

    Updates

--- precache-manifest.bb92832e11eb0eeeb39fbec606a250f4.js
+++ precache-manifest.bb92832e11eb0eeeb39fbec606a250f4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "78474b5736d9c809a9dba0954f7b8fa0",
+    "revision": "12cb47469ebdcf6119707dcb39e257b7",
     "url": "/ChatBot-Acai/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ChatBot-Acai/static/css/2.0441cb4d.chunk.css"
   },
   {
-    "revision": "7961b70c8d6b457b523d",
+    "revision": "00261ad90b242699088f",
     "url": "/ChatBot-Acai/static/css/main.8740ae2e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ChatBot-Acai/static/js/2.9c8fb73d.chunk.js"
   },
   {
-    "revision": "7961b70c8d6b457b523d",
-    "url": "/ChatBot-Acai/static/js/main.fb35c1ac.chunk.js"
+    "revision": "00261ad90b242699088f",
+    "url": "/ChatBot-Acai/static/js/main.2607e7e0.chunk.js"
   },
   {
     "revision": "bd88e93d35c10383a6c9",
